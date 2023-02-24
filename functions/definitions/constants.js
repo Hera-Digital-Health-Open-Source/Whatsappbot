f84a7ad17e3ac34632d840d29e4419c90f16a901
Bing Wen <tan_bing_wen@cpf.gov.sg>
2023-02-24 18:12:02 +0800

    Added msgs

---
+++ functions/definitions/constants.js
@@ -0,0 +1,62 @@
+WELCOME_MSG = `Hi, Merhaba, مرحباً !
+- Which organization are you from?
+- Hangi kuruluştansın?
+- من أي منظمة أنت؟`
+
+SELECTION_PROMPT_MSG = `Which resource would you like to add?
+Hangi hizmeti eklemek istersiniz?
+ما هو الغرض الذي ترغب في إضافته؟
+
+Enter the number.
+Numarayı girin.
+أدخل الرقم`
+
+SELECTION_MSG = `1. Medikal Destek - Medical support - الدعم الطبي
+
+2. Yemek - Food - الطعام
+
+3. Barınma (Çadır, Konteyner, Prefabrik Ev)
+Accommodation (Tent, container, prefabricated house)
+مكان مبيت (خيمة ، حاوية ، منزل جاهز)
+
+4. Psikososyal Destek - Psychosocial Support - الدعم النفسي
+
+5. Rehabilitasyon - Rehabilitation - إعادة تأهيل
+
+6. Kıyafet Dağıtımı - Clothe Distribution - توزيع الملابس
+
+7. Tuvalet WC - Toilet - مرحاض
+
+8. Seyyar Tuvalet WC - Mobile Toilet - مرحاض متنقل
+
+9. Banyo - Shower - استحمام
+
+10. Hayvan Mama Dağıtımı - Animal Food Distribution - توزيع أغذية للحيوانات
+
+11. Veteriner - Veterinary - طبيب بيطري
+
+12. Hijyen Kiti Dağıtımı - Hygiene Kit Distribution - توزيع أدوات نظافة
+
+13. Jenaratör ve Şarj İstasyonu - Power Plant and Changing Station - مولد و محطة كهرباء
+
+14. Depo Koordinasyon Merkezi - Storage Coordination Center - مركز تنسيق التخزين
+
+15. Temiz su - Clean Water - ماء نظيف
+
+16. Çocuklar için Alan - Child Safe Space - مساحة آمنة للأطفال
+
+17. Çocuk Oyun Alanı - Child Game Space - مساحة ألعاب للأطفال
+
+18. Kadın ve Kız Çocukları için Hijyen Kiti Dağıtımı - Women and Girls Hygiene Kit Distribution - توزيع أدوات النظافة للنساء والفتيات
+
+19. Diğer - Others - أخرى`
+
+UNDO_MSG = `- If you keyed in a wrong number, type “Undo”.
+Else, Share your location now.
+
+- Yanlış bir numara girdiyseniz, “Geri Al” yazın.
+Aksi takdirde, konumunuzu şimdi paylaşın.
+
+- إذا أدخلت رقمًا خاطئًا ، فاكتب ”تراجع”.
+عدا ذلك ، شارك موقعك الآن.`
+
